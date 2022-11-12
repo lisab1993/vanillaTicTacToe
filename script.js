2b34d784050e3175775eb051cbdc8f6a2358f38d
lisab1993 <brown.lisa999@gmail.com>
2022-11-11 17:53:52 -0800

    all connected

---
+++ script.js
@@ -0,0 +1,7 @@
+let clickBtn = document.querySelector('#click_btn')
+let output = document.querySelector('#output')
+
+
+clickBtn.addEventListener('click', function() {
+    output.innerText = "Hello World"
+})
